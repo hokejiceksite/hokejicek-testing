7a7ad808b9a0303d159db13edddec5d05f9b5570
hokejiceksite <bilcusadam@gmail.com>
2025-11-07 12:55:48 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBtf3bEzJU5et-Q11j38QHNMHyOeoOb98A",
+  authDomain: "hokejicek-2fe46.firebaseapp.com",
+  databaseURL: "https://hokejicek-2fe46-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hokejicek-2fe46",
+  storageBucket: "hokejicek-2fe46.firebasestorage.app",
+  messagingSenderId: "980610969771",
+  appId: "1:980610969771:web:1b03ca9715204ef6bd47ee",
+  measurementId: "G-M0FJ2RMY9V"
+};
+</script>
